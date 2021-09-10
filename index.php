3ba2b2d1cd3de49c1109e4534ab653f678930b14
Chimake <dev.chimaobi@gmail.com>
2021-09-10 19:00:54 +0800

    hot fix

--- index.php
+++ index.php
@@ -125,34 +125,22 @@
                                 <!-- mainmenu begin -->
                                 <ul id="mainmenu">
                                     <li>
-                                        <a href="03_grey-index.html">Home<span></span></a>
-                                        <ul>
-                                            <li><a href="03_grey-index.html">Homepage 1</a></li>
-                                            <li><a href="03_grey-index-2.html">Homepage 2</a></li>
-                                            <li><a href="03_grey-index-3.html">Homepage 3</a></li>
-                                        </ul>
+                                        <a href="#home">Home<span></span></a>
+                                        
                                     </li>
                                     <li>
-                                        <a href="03_grey-explore.html">Explore<span></span></a>
-                                        <ul>
-                                            <li><a href="03_grey-explore.html">Explore</a></li>
-                                            <li><a href="03_grey-explore-2.html">Explore 2</a></li>
-                                            <li><a href="03_grey-collection.html">Collections</a></li>
-                                            <li><a href="03_grey-item-details.html">Item Details</a></li>
-                                        </ul>
+                                        <a href="#home">Story<span></span></a>
+                                        
                                     </li>
                                     <li>
-                                        <a href="03_grey-author.html">Author<span></span></a>
-                                        <ul>
-                                            <li><a href="03_grey-author.html">Author</a></li>
-                                            <li><a href="03_grey-wallet.html">Wallet</a></li>
-                                            <li><a href="03_grey-create.html">Create</a></li>
-                                            <li><a href="03_grey-login.html">Login</a></li>
-                                        </ul>
-                                    </li>                                    
-                                    <li>
-                                        <a href="03_grey-activity.html">Activity<span></span></a>
+                                        <a href="#home">Roadmap<span></span></a>
+                                        
                                     </li>
+                                    <li>
+                                        <a href="#home">Team<span></span></a>
+                                        
+                                    </li>
+                                  
                                 </ul>
                                 <!-- mainmenu close -->
                                 <div class="menu_side_area">
